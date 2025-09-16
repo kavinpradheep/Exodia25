cf6747f2cdefc46a9cc5b06dd53c77d5b20cbbcb
kavinpradheep <kavinpradheep8@gmail.com>
2025-09-16 22:18:29 +0530

    changed the bg color for timeline

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -142,7 +142,7 @@ function DayTimeline({ title, events }) {
 
 export default function Timeline() {
   return (
-    <section id="timeline" style={{ background: "#00111b", color: "white", padding: "40px 16px" }}>
+    <section id="timeline" style={{ background: "#000F1F", color: "white", padding: "40px 16px" }}>
       <div style={{ maxWidth: 1150, margin: "0 auto" }}>
         <h2
           style={{
